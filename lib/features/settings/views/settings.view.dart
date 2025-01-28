da138aa1b6a63c01ca328a0f8861a91defe64598
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-01-28 09:46:20 -0300

    feat(settings): add appbar

--- lib/features/settings/views/settings.view.dart
+++ lib/features/settings/views/settings.view.dart
@@ -6,6 +6,9 @@ class SettingsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
       body: Placeholder(),
     );
   }
